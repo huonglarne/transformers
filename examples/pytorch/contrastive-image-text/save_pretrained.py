af4c042c349a955a046068cd6f81538b4afcbc89
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2023-04-18 16:27:04 +0900

    fix

--- examples/pytorch/contrastive-image-text/save_pretrained.py
+++ examples/pytorch/contrastive-image-text/save_pretrained.py
@@ -5,18 +5,19 @@ from transformers import (
     AutoImageProcessor
 )
 
-model = VisionTextDualEncoderModel.from_vision_text_pretrained(
-    "openai/clip-vit-base-patch32", "roberta-base"
-)
-
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
 # read model_names.txt
 with open("model_names.txt", "r") as f:
     model_names = f.read().splitlines()
 
-for model_name in model_names:
-    # load the model and processor
+for line in model_names:
+    model_name = line.split()[0]
+
+    model = VisionTextDualEncoderModel.from_vision_text_pretrained(
+        model_name, "roberta-base"
+    )
+
     image_processor = AutoImageProcessor.from_pretrained(model_name)
     processor = VisionTextDualEncoderProcessor(image_processor, tokenizer)
 
